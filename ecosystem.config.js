5d9169d8e72848aaa0ee5d6bf27df9d03aaf5049
Cesar Enrique Gonzalez Carvajal <cesargonzalez390@gmail.com>
2026-02-10 13:04:13 -0500

    feat: Add PM2 ecosystem configuration and an environment variable template.

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: 'ecomishop',
+        script: 'dist/main.js',
+        instances: 1,
+        exec_mode: 'fork',
+        watch: false,
+        max_memory_restart: '450M',
+        env: {
+            NODE_ENV: 'production',
+            PORT: 3000
+        }
+    }]
+};
